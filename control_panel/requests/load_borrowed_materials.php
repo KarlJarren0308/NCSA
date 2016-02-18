13eead63ea1068f3ff7135dd6453f9e13022b99f
Karl Macadangdang <karljarren0308@yahoo.com>
2016-02-19 05:57:55 +0800

    File for listing borrowed materials

--- control_panel/requests/load_borrowed_materials.php
+++ control_panel/requests/load_borrowed_materials.php
@@ -4,31 +4,86 @@
 
     require_once('connection.php');
     require_once('control_panel_settings.php');
+    require_once('functions.php');
 
     $connection = new Connection();
     $connection->open();
+
+    $connection2 = new Connection();
+    $connection2->open();
 
     $settings = new ControlPanelSettings('../');
 
     $id = $connection->escape_string($_POST['id']);
     $penalty = 0;
 
+    $connection->query("SELECT * FROM accounts WHERE Account_ID='$id'");
+    $rowAccounts = $connection->fetch_assoc();
+
+    if($rowAccounts['Account_Type'] == 'Student') {
+        $days = $settings->getSetting('studentLoanPeriod');
+    } else {
+        $days = $settings->getSetting('facultyLoanPeriod');
+    }
+
     $connection->query("SELECT *, borrow_details.Status AS Borrow_Details_Status FROM borrow_details INNER JOIN borrow ON borrow_details.Borrow_ID=borrow.Borrow_ID INNER JOIN barcodes ON borrow_details.Barcode_Number=barcodes.Barcode_Number INNER JOIN materials ON barcodes.Material_ID=materials.Material_ID WHERE borrow.Borrowers_ID='$id'");
 
     while($row = $connection->fetch_assoc()) {
-        if(strtotime(date('Y-m-d')) > strtotime($row['Due_Date'])) {
-            $penalty = ((strtotime(date('Y-m-d')) - strtotime($row['Due_Date'])) / 86400) * (double) $settings->getSetting('penalty');
-        } else {
-            $penalty = 0;
+        $dueDate = date('F d, Y', strtotime('+' . $days . ' days', strtotime($row['Date_Borrowed'])));
+        $dueDays = ceil((strtotime($dueDate) - strtotime($row['Date_Borrowed'])) / 86400);
+        $i = 1;
+
+        while($i <= $dueDays) {
+            $currentDate = date('Y-m-d', strtotime('+' . $i . ' days', strtotime($row['Date_Borrowed'])));
+
+            if(isWeekend($currentDate)) {
+                $dueDays++;
+                $dueDate = nextDay($dueDate);
+            } else {
+                if(isHoliday($connection2, $currentDate)) {
+                    $dueDays++;
+                    $dueDate = nextDay($dueDate);
+                }
+            }
+
+            $i++;
         }
+
+        $duedueDate = $dueDate;
+        $duedueDays = ceil((strtotime(date('Y-m-d')) - strtotime($duedueDate)) / 86400);
+        $j = 1;
+
+        while($j <= $duedueDays) {
+            $currentDate = date('Y-m-d', strtotime('+' . $j . ' days', strtotime($duedueDate)));
+
+            if(isWeekend($currentDate)) {
+                $duedueDays++;
+                $duedueDate = nextDay($duedueDate);
+            } else {
+                if(isHoliday($connection2, $currentDate)) {
+                    $duedueDays++;
+                    $duedueDate = nextDay($duedueDate);
+                }
+            }
+
+            $j++;
+        }
+
+        $penalty = ceil((strtotime(date('Y-m-d')) - strtotime($duedueDate)) / 86400) * (double) $settings->getSetting('penalty');
 
         echo '<tr>';
         echo '<td>' . $row['Barcode_Number'] . '</td>';
         echo '<td>' . $row['Material_Title'] . '</td>';
         echo '<td>' . $row['Collection_Type'] . '</td>';
         echo '<td>' . date('F d, Y', strtotime($row['Date_Borrowed'])) . '</td>';
-        echo '<td>' . date('F d, Y', strtotime($row['Due_Date'])) . '</td>';
-        echo '<td>' . $penalty . '</td>';
+
+        if($row['Borrow_Details_Status'] == 'active') {
+            echo '<td>' . date('F d, Y', strtotime($dueDate)) . '</td>';
+        } else {
+            echo '<td>' . date('F d, Y', strtotime($row['Final_Due_Date'])) . '</td>';
+        }
+
+        echo '<td>' . ($penalty >= 0 ? $penalty : 0) . '</td>';
         
         if(isset($_SESSION['account_username'])) {
             if($row['Borrow_Details_Status'] == 'active') {
@@ -36,10 +91,13 @@
             } else {
                 echo '<td class="align-center"><a href="javascript:void(0);" class="button mini-button beautify" data-role="hint" data-hint="Returned" data-hint-background="#dc4fad" data-hint-color="#ffffff" data-hint-mode="2" data-hint-position="top"><span class="mif mif-checkmark"></span></a></td>';
             }
+        } else {
+            echo '<td></td>';
         }
 
         echo '</tr>';
     }
 
+    $connection2->close();
     $connection->close();
 ?>
